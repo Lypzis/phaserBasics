3555ee4fa969bafc0aa411f1a6a562fb3c98b887
Victor_Piccoli <victorvp2008@hotmail.com>
2018-03-12 13:48:26 -0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,21 +12,26 @@ let soundTrack,
     cursors,
     bullets, //group
     bulletTime = 0,
-    fireButton;
+    fireButton,
+    enemies,
+    enemy;
+
+
 
 const mainState = {
     //preloads images and sounds into the game
-    preload(){
-   
+    preload() {
+
         game.load.audio('soundTrack', './audio/mars.mp3');
         game.load.image('nightField', './assets/nightSky.png');
         game.load.image('player', './assets/spaceShip.png');
         game.load.image('bullet', './assets/bullet.png'); //16x16
+        game.load.image('enemy', './assets/enemy.png');
 
     },
 
     //creates cenario for the game(physics, positioning, ...)
-    create(){
+    create() {
 
         //background cenario
         spaceField = game.add.tileSprite(0, 0, 800, 600, 'nightField');
@@ -39,7 +44,7 @@ const mainState = {
 
         //positioning player on the screen
         player = game.add.sprite(game.world.centerX, game.world.centerY + 200, 'player');
-        player.scale.x = 0.1; 
+        player.scale.x = 0.1;
         player.scale.y = 0.1;
 
         //adding physics to the game
@@ -51,38 +56,82 @@ const mainState = {
         //the bullets of our spaceShip
         bullets = game.add.weapon(30, 'bullet'); //group, when something needs to be recycled(remember: memory garbage collection)
         bullets.bulletAngleOffset = 90;
-        bullets.
         bullets.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
         bullets.bulletSpeed = 400;
         bullets.trackSprite(player, 35, 0);
-        
+        bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        bullets.enableBody = true;
+
         fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
+        enemies = game.add.group();
+        enemies.enableBody = true;
+        enemies.physicsBodyType = Phaser.Physics.ARCADE;
+
+        createEnemies();
     },
 
-    update(){   
+    update() {
+
+        game.physics.arcade.overlap(bullets, enemies, collisionHandler, null, this);
+
         //playing soundtrack, loop = true
-        soundTrack.play("", 0, 10, true, false);
+        //soundTrack.play("", 0, 7, true, false);
 
         //makes cenario moves in vertical loop
         spaceField.tilePosition.y += backgroundSpeed;
-        
+
         //iddle
         player.body.velocity.x = 0;
 
-        if(cursors.left.isDown){
+        if (cursors.left.isDown) {
             player.body.velocity.x = -350;
-        }  
-        else if(cursors.right.isDown){
+        }
+        else if (cursors.right.isDown) {
             player.body.velocity.x = 350;
         }
 
         //fire
-        if (fireButton.isDown){
+        if (fireButton.isDown) {
             bullets.fire();
         }
 
     }
+}
+
+function createEnemies() {
+    for (let y = 0; y < 4; ++y) {
+        for (let x = 0; x < 10; ++x) {
+            enemy = enemies.create(x * 200, y * 200, 'enemy');
+            enemy.anchor.setTo(0.5, 0.5);
+        }
+    }
+    enemies.scale.x = 0.15;
+    enemies.scale.y = 0.15;
+
+    enemies.x = 100;
+    enemies.y = 50;
+
+    let tween = game.add.tween(enemies).to(
+        { x: 450 },
+        2000,
+        Phaser.Easing.Linear.None,
+        true,
+        0, 
+        1000,
+        true
+    );
+
+    tween.onRepeat.add(descend, this);
+}
+
+function descend(){
+    enemies.y += 10;
+}
+
+function collisionHandler(){
+    bullets.kill();
+    enemy.kill();
 }
 
 game.state.add('mainState', mainState);
